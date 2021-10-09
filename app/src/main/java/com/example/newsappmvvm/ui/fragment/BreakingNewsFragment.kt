2ac434ebccc5c781179be4bd287836b8aed892e8
AhmedGhanam <yghnam334455@gmail.com>
2021-10-09 08:11:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/newsappmvvm/ui/fragment/BreakingNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.newsappmvvm.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.newsappmvvm.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_break_news){
+
+}
